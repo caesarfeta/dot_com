251030a28d617a76d6b0b91277acdf5bef9b81d2
Adam Tavares <jlampkin@Jesse-Lampkins-iMac.local>
2013-10-23 14:29:11 -0400

    changed permissions.  Improved install scripts.

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
+#!/bin/bash
 # Install python dependencies
 sudo python build/src/Jinja2-2.7.1/setup.py install
 sudo python build/src/Markdown-2.3.1/setup.py install
 
+# Build Javascript libraries
+javascript/lib/build.sh
+
 # Grab submodules
 git submodule init
 git submodule update
+
+# Build pages
+build/build.sh
--- javascript/lib/build.sh
+++ javascript/lib/build.sh
@@ -1,3 +1,3 @@
-#/bin/bash
+#!/bin/bash
 python make.py
 closure-compiler-cli/closure_compiler_cli.py --level=simple --file=lib.js > lib.min.js
