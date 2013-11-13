fa07ee13bc7f3504a5d732a6fe9f54946d18be86
Adam Tavares <adamtavares@KarateTown.local>
2013-11-13 08:46:43 -0500

    fleshing out lsystems renderer.

--- code/l_systems/LSYS.js
+++ code/l_systems/LSYS.js
@@ -22,9 +22,7 @@ LSYS.Sys = function( _iter, _angle, _start ) {
 		var map = arguments[i].split('=');
 		this.rules[map[0]] = map[1];
 	}
-	
-	console.log( this.rules );
-	
+		
 	return {
 		
 		/**
@@ -74,7 +72,6 @@ LSYS.Sys = function( _iter, _angle, _start ) {
 
 LSYS.Renderer = function( _canvasId ) {
 	this.canvas = document.getElementById( _canvasId );
-	this.RAD = Math.PI/180.0;
 	this.constants = {
 		'+': 1, // counter clockwise
 		'-': 1, // clockwise
@@ -83,11 +80,15 @@ LSYS.Renderer = function( _canvasId ) {
 		'C': 1, // color
 	};
 	
-	return {
-		polarToCart: function( _radius, _angle ) {
-			return [ _radius*Math.cos( _angle ), _radius*Math.sin( _angle ) ];
-		}
-	}
+	this.toRad = function( _degrees ) {
+		return _degrees * Math.PI / 180;
+	};
+	
+	this.toCart = function( _radius, _angle ) {
+		return [ _radius * Math.cos( this.toRad(_angle) ), _radius * Math.sin( this.toRad(_angle) ) ];
+	};
+	
+	console.log( this.toCart( 1, 90 ) );
 }
 
 
@@ -103,12 +104,35 @@ LSYS.TwoD = function( _canvasId ){
 		draw: function( _input, _angle ) {
 			var angle = _angle;
 			var coords = [];
-			var coords.push( [0,0] );
+			coords.push( [0,0] );
 			var chars = _input.split('');
 			for ( var i in chars ) {
-				console.log( chars[i] );
+				
+				//------------------------------------------------------------
+				//  Draw LOGO style
+				//------------------------------------------------------------
+				if ( chars[i] in this.constants ) {
+					var last = coords[ coords.length-1 ];
+					var next = this.toCart( 1, this.toRad( angle ) );
+					var x = last[0] + next[0];
+					var y = last[1] + next[1];
+					coords.push( [x,y] );
+				}
+				else {
+					switch ( chars[i] ) {
+						case '+':
+							angle += angle;
+							break;
+							
+						case '-':
+							angle -= angle;
+							break;
+					}
+				}
 			}
-		}
+			console.log( coords );
+		},
+		constants: this.constants
 	}
 }
 LSYS.TwoD.prototype = Object.create( LSYS.Renderer.prototype );
