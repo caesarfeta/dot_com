9849320801b7d1380564a0048f625307f7439a55
caesarfeta <adamtavares@gmail.com>
2014-01-02 12:29:08 -0500

    Started work on publishing system.

--- publish.sh
+++ publish.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 STAGE_DIR="/var/www/stage/dot_com"
 PUB_DIR="/var/www/pub/dot_com"
-FTP_URL=""
-FTP_USER=""
-FTP_PASS=""
-FTP_DIR=""
+DIFF_DIR="/var/www/diff/dot_com"
 
-# Build Staging Directory
+function pause() {
+	read -p "$*"
+}
+
+echo "--Building Stage Directory--"
 mkdir -p $STAGE_DIR
 rsync -avz \
+	--exclude '.DS_Store' \
 	--exclude 'build' \
 	--exclude '*.sh' \
 	--exclude '.git*' \
@@ -20,9 +22,27 @@ rsync -avz \
 	--exclude 'third_party/three.js/examples' \
 	. $STAGE_DIR
 
-# Check Pub Directory
+pause "Check http://127.0.0.1/stage/dot_com and if you like what you see press [Enter] to publish"
+
+echo "--Checking Publish Directory--"
 if [ ! -d "$PUB_DIR" ]; then
 	mkdir -p $PUB_DIR
 fi
 
-# Compare the Contents
+echo "--Checking Diff Directory--"
+if [ ! -d "$DIFF_DIR" ]; then
+	mkdir -p $DIFF_DIR
+fi
+
+echo "--Comparing Stage and Publish Directory--"
+rsync -rvcm --compare-dest=$PUB_DIR/ $STAGE_DIR/ $DIFF_DIR/
+
+pause "Check ${DIFF_DIR} to see files that need to be copied over FTP.  After copying press [ENTER] to clean-up and update your publish directory"
+
+echo "--Updating Publish Directory--"
+rsync -avz $STAGE_DIR $PUBDIR
+
+echo "--Cleaning Diff Directory--"
+rm -rf $DIFF_DIR
+
+echo "--DONE--"
