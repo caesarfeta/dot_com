f48cd8b14956182b737c06c159592b18c1bff572
caesarfeta <adamtavares@gmail.com>
2014-02-06 22:51:19 -0500

    Getting ready for site launch.  Slowly but surely.

--- code/l_systems/l_systems.js
+++ code/l_systems/l_systems.js
@@ -1,3 +1,12 @@
-var canvas = new AutoCanvas( 'canvas', 100 );
-//var dragon = new LSYS.DragonCurve( 'canvas' );
-var hexSierp = new LSYS.HexagonSierpinski( 'canvas' );
+$( document ).ready( function(){
+	var sh =$( '#code' ).jssh({
+		"background-color": '#AAA',
+		"padding": 50,
+		"vars": {
+			time: 1,
+			train: 10
+		}
+	}).data('#code');
+//	var dragon = new LSYS.DragonCurve( sh.canvas );
+	var hexSierp = new LSYS.HexagonSierpinski( sh.canvas );
+});
