8ad21c481880271a4e0d18b78eb450bb68ab0681
caesarfeta <adamtavares@gmail.com>
2014-02-24 18:41:16 -0500

    Small improvements are still improvements.

--- code/l_systems/l_systems.js
+++ code/l_systems/l_systems.js
@@ -1,12 +1,11 @@
 $( document ).ready( function(){
 	var sh =$( '#code' ).jssh({
 		"background-color": '#AAA',
-		"padding": 50,
+		"padding": 100,
 		"vars": {
 			time: 1,
 			train: 10
 		}
 	}).data('#code');
 	var dragon = new LSYS.DragonCurve( sh.canvas );
-//	var hexSierp = new LSYS.HexagonSierpinski( sh.canvas );
 });
