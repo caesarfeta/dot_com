796744a1db6e03920086fc71a35acf209eb0efae
caesarfeta <adamtavares@gmail.com>
2014-03-13 14:42:50 -0400

    Played with SPARQL a bit.  I'm starting to get pretty goooood at it.

--- code/specierch/specierch.js
+++ code/specierch/specierch.js
@@ -5,13 +5,20 @@
 //  Build the search box
 //------------------------------------------------------------
 var html = '\
+<div id="specierch">\
 	<div id="search">\
 		<input id="input" type="text" placeholder="Genus Name">\
 	</div>\
 	<div id="imgShell"><div id="imgBox"></div></div>\
+	<div id="options">\
+		<a id="aboutClick" class="bigger" href="about">?</a>\
+	</div>\
+	<div id="about">\
+		<p><a href="../../story/dbpedia">Learn more about how specierch works.</a></p>\
+	</div>\
 	<div id="hint">\
 		<p>Welcome to specierch.</p>\
-		<p>It\'s fun to look at similar species.</p>\
+		<p>It\'s fun to look at images of living organisms.  Especially when they\'re closely related.</p>\
 		<p>Type a genus name into the input field, press Enter,\
 		and wait for the images of species belonging to the genus to come streaming in.</p>\
 		<p>Here\'s some sample genus names:\
@@ -22,7 +29,7 @@ var html = '\
 		<p>I am searching. DbPedia can be really slow sometimes.</p>\
 		<p>Thank you for waiting.</p>\
 	</div>\
-';
+</div>';
 $( 'body' ).append( html );
 
 //------------------------------------------------------------
@@ -32,16 +39,17 @@ $( '#search' ).css({
 	'margin-left': $( '#search' ).outerWidth()/2 * -1
 });
 
-//------------------------------------------------------------
-//  Wait
-//------------------------------------------------------------
 centerId( 'wait' );
-
-//------------------------------------------------------------
-//  Hint
-//------------------------------------------------------------
 centerId( 'hint' );
+centerId( 'about' );
 hideWait();
+hideAbout();
+$( '#aboutClick' ).on( 'touchstart click', function( _e ) {
+	_e.preventDefault();
+	hideWait();
+	hideHint();
+	( $('#about').is(':visible') ) ? hideAbout() : showAbout();
+})
 
 //------------------------------------------------------------
 //  imgBox
@@ -85,6 +93,12 @@ function hideHint() {
 function showHint() {
 	$('#hint').show();
 }
+function hideAbout() {
+	$( '#about' ).hide();
+}
+function showAbout() {
+	$('#about').show();
+}
 function imgShellSize() {
 	$('#imgShell').width( $(window).width() );
 	$('#imgShell').height( $(window).height() );
@@ -92,6 +106,10 @@ function imgShellSize() {
 function markSearch( _name ) {
 	$( '#imgBox' ).append( '<div class="search_mark">' + _name + '</div>' );
 }
+
+//------------------------------------------------------------
+//  Retrieve images belonging to a particular genus
+//------------------------------------------------------------
 function genusImgs( _genus, _output ) {
 	_genus = _genus.toLowerCase().capitalize();
 	var url = "http://dbpedia.org/sparql";
@@ -131,3 +149,127 @@ function genusImgs( _genus, _output ) {
 		}
 	});
 }
+/*
+
+//------------------------------------------------------------
+//  Grab images belonging to genus
+//------------------------------------------------------------
+SELECT ?o
+WHERE {
+	?s 	dbpedia2:genus 	'Apis'@en;
+		foaf:depiction 	?o
+}
+
+//------------------------------------------------------------
+//  Common name to genus
+//------------------------------------------------------------
+SELECT ?genus
+WHERE {
+	?s 	rdfs:label 		"Honey bee"@en;
+		dbpedia2:genus 	?genus
+}
+
+//------------------------------------------------------------
+//  'Like' type search... this can be very slow
+//------------------------------------------------------------
+SELECT ?o
+WHERE {
+	?s rdfs:label ?o
+	FILTER regex( ?o, "bee", "i" )
+}
+
+//------------------------------------------------------------
+//  'Like' type search... for only organisms
+//------------------------------------------------------------
+SELECT ?o
+WHERE {
+	?s 	dbpedia2:regnum ?kingdom;
+		rdfs:label ?o
+		FILTER regex( ?o, "bee", "i" )
+}
+
+//------------------------------------------------------------
+//  'Like' type search... only for  organisms get ontology
+//------------------------------------------------------------
+SELECT ?name, ?o
+WHERE {
+	?s 	dbpedia2:regnum ?any;
+		rdfs:label?name
+		FILTER regex( ?name, "bee", "i" )
+    ?name ?p ?o
+}
+
+//------------------------------------------------------------
+//  Official species only english?
+//------------------------------------------------------------
+SELECT ?name, ?animal
+WHERE {
+	?s 	dbpedia2:regnum ?hasValue;
+		rdfs:label?name
+		FILTER regex( ?name, "bee", "i" )
+		FILTER ( langMatches( lang( ?name ), "EN" ))
+	?animal ?p ?name
+}
+
+//------------------------------------------------------------
+//  Where to go from here?
+//------------------------------------------------------------
+SELECT ?name, ?animal, ?p
+WHERE {
+	?s 	dbpedia2:regnum ?hasValue;
+		rdfs:label?name
+		FILTER regex( ?name, "bee", "i" )
+		FILTER ( langMatches( lang( ?name ), "EN" ))
+	?animal ?p ?name
+}
+
+//------------------------------------------------------------
+//  Okay let's make sure what gets returned is the same property
+//------------------------------------------------------------
+SELECT ?animal, ?kingdom, ?phylum, ?order, ?img
+WHERE {
+	?s 	dbpedia2:regnum ?hasValue;
+		rdfs:label?name
+		FILTER regex( ?name, "bee", "i" )
+		FILTER ( langMatches( lang( ?name ), "EN" ))
+	?animal dbpedia2:name ?name;
+		dbpedia2:regnum ?kingdom;
+		foaf:depiction ?img
+}
+
+//------------------------------------------------------------
+//  HAHAHAHAAHAH I've done it!
+//------------------------------------------------------------
+//------------------------------------------------------------
+//  I'm going to build the search around this query now.
+//------------------------------------------------------------
+SELECT  ?animal, ?name, ?kingdom, ?order, ?phylum, ?class, ?family, ?genus, ?species, ?subspecies, ?img, ?abstract
+WHERE {
+	?s 	dbpedia2:regnum ?hasValue;
+		rdfs:label ?name
+		FILTER regex( ?name, "bee", "i" )
+		FILTER ( langMatches( lang( ?name ), "EN" ))
+	?animal dbpedia2:name ?name
+	OPTIONAL { ?animal dbpedia2:regnum ?kingdom . }
+	OPTIONAL { ?animal dbpedia2:ordo ?order . }
+	OPTIONAL { ?animal dbpedia2:phylum ?phylum . }
+	OPTIONAL { ?animal dbpedia2:classis ?class . }
+	OPTIONAL { ?animal dbpedia2:familia ?family . }
+	OPTIONAL { ?animal dbpedia2:genus ?genus . }
+	OPTIONAL { ?animal dbpedia2:species ?species . }
+	OPTIONAL { ?animal dbpedia2:subspecies ?subspecies . }
+	OPTIONAL { ?animal foaf:depiction ?img . }
+	OPTIONAL { 
+		?animal <http://dbpedia.org/ontology/abstract> ?abstract
+		FILTER ( langMatches( lang( ?abstract ), "EN" ))
+	}
+}
+
+dbpedia:ontology/class [http]	:Insect [http]
+dbpedia:ontology/family [http]	:Apidae [http]
+dbpedia:ontology/family [http]	:Apinae [http]
+dbpedia:ontology/kingdom [http]	:Animal [http]
+dbpedia:ontology/order [http]	:Hymenoptera [http]
+dbpedia:ontology/phylum [http]	:Arthropod [http]
+
+*/
