fb26b8768af0df6080e8882365e8413b14fde51f
caesarfeta <adamtavares@gmail.com>
2014-09-24 17:39:14 -0400

    "Homepage" is now broken up across lots of pages.

--- build/buildHome.py
+++ build/buildHome.py
@@ -2,6 +2,7 @@
 
 import buildLib
 from time import gmtime, strftime
+import os
 
 def build():
 	# Get the list of published stories
@@ -15,13 +16,34 @@ def build():
 	# Get the analytics tracking code
 	data["tracking"] = buildLib.loadTemplate( "gaTrackingCode" ).encode('utf8')
 	
+	ind = 1
+	pn = 0;
+	file = "../index.html";
+	
+	# More button
+	data["more"] = '<a href="index2.html">++</a>'
+	data["less"] = ""
 	for page in pages:
 		page = "../" + page + "/preview.html.snpt"
 		f = open( page, "r" )
 		data["previews"].append( f.read() )
 		f.close()
-	
-	# Build the homepage
-	buildLib.parseTemplate( "index", "../index.html", data )
-
+		if ind%5==0:
+			# Build the homepages
+			if pn > 0:
+				file="../index"+str(pn)+".html"
+				if pn > 1:
+					data["less"] = '<a href="index'+str(pn-1)+'.html">--</a>'
+				else:
+					data["less"] = '<a href="index.html">--</a>'
+				data["more"] = '<a href="index'+str(pn+1)+'.html">++</a>'
+			buildLib.parseTemplate( "index", file, data )
+			data["previews"] = []
+			pn += 1
+		ind += 1
+		if ind == len(pages)+1:
+			file="../index"+str(pn)+".html"
+			data["less"] = '<a href="index'+str(pn-1)+'.html">--</a>'
+			data["more"] = ''
+			buildLib.parseTemplate( "index", file, data )
 build()
